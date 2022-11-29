2c2ea21b095a3f5310fb9d6cdda3c5a1d4ed0a8c
Tim Harder <radhermit@gmail.com>
2022-11-28 20:45:35 -0700

    pkg_ebuild: add accessor functions for more ebuild pkg attrs

--- pkg_ebuild.go
+++ pkg_ebuild.go
@@ -4,6 +4,10 @@ package pkgcraft
 // #include <pkgcraft.h>
 import "C"
 
+import (
+	"errors"
+)
+
 type EbuildPkg struct {
 	*BasePkg
 }
@@ -11,6 +15,40 @@ type EbuildPkg struct {
 // Return a package's path.
 func (p *EbuildPkg) Path() string {
 	s := C.pkgcraft_pkg_ebuild_path(p.ptr)
+	defer C.pkgcraft_str_free(s)
+	return C.GoString(s)
+}
+
+// Return a package's ebuild file content.
+func (p *EbuildPkg) Ebuild() (string, error) {
+	s := C.pkgcraft_pkg_ebuild_ebuild(p.ptr)
+	if s != nil {
+		defer C.pkgcraft_str_free(s)
+		return C.GoString(s), nil
+	} else {
+		s := C.pkgcraft_last_error()
+		defer C.pkgcraft_str_free(s)
+		return "", errors.New(C.GoString(s))
+	}
+}
+
+// Return a package's description.
+func (p *EbuildPkg) Description() string {
+	s := C.pkgcraft_pkg_ebuild_description(p.ptr)
+	defer C.pkgcraft_str_free(s)
+	return C.GoString(s)
+}
+
+// Return a package's slot.
+func (p *EbuildPkg) Slot() string {
+	s := C.pkgcraft_pkg_ebuild_slot(p.ptr)
+	defer C.pkgcraft_str_free(s)
+	return C.GoString(s)
+}
+
+// Return a package's subslot.
+func (p *EbuildPkg) Subslot() string {
+	s := C.pkgcraft_pkg_ebuild_subslot(p.ptr)
 	defer C.pkgcraft_str_free(s)
 	return C.GoString(s)
 }
