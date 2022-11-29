614724f718ae06dfd7b6eaf01e7651cf82b99be5
Tim Harder <radhermit@gmail.com>
2022-11-28 19:37:33 -0700

    repo: use Pkgs() channel for pkg iteration over Repo types

--- repo.go
+++ repo.go
@@ -13,6 +13,7 @@ type Repo interface {
 	Path() string
 	IsEmpty() bool
 	String() string
+	Pkgs() <-chan *BasePkg
 }
 
 type BaseRepo struct {
@@ -43,34 +44,25 @@ func (r *BaseRepo) String() string {
 	return r.Id()
 }
 
-type repo_pkg_iter struct {
-	ptr *C.RepoPkgIter
-	next *BasePkg
-}
+// Return a channel iterating over the packages of a repo.
+func (r *BaseRepo) Pkgs() <-chan *BasePkg {
+	pkgs := make(chan *BasePkg)
 
-// Return true if the repo iterator contains another package, false otherwise.
-func (i *repo_pkg_iter) HasNext() bool {
-	ptr := C.pkgcraft_repo_iter_next(i.ptr)
-	if ptr == nil {
-		i.next = nil
-		return false
-	} else {
-		i.next = pkg_from_ptr(ptr)
-		return true
-	}
-}
+	go func() {
+		iter := C.pkgcraft_repo_iter(r.ptr)
+		for {
+			ptr := C.pkgcraft_repo_iter_next(iter)
+			if ptr != nil {
+				pkgs <- pkg_from_ptr(ptr)
+			} else {
+				break
+			}
+		}
+		close(pkgs)
+		C.pkgcraft_repo_iter_free(iter)
+	}()
 
-// Return the next package in the iterator.
-func (i *repo_pkg_iter) Next() *BasePkg {
-	return i.next
-}
-
-// Return a new package iterator for a repo.
-func (r *BaseRepo) NewPkgIterator() Iterator[*BasePkg] {
-	ptr := C.pkgcraft_repo_iter(r.ptr)
-	iter := &repo_pkg_iter{ptr, nil}
-	runtime.SetFinalizer(iter, func(i *repo_pkg_iter) { C.pkgcraft_repo_iter_free(i.ptr) })
-	return iter
+	return pkgs
 }
 
 type FakeRepo struct {
--- repo_ebuild.go
+++ repo_ebuild.go
@@ -4,45 +4,32 @@ package pkgcraft
 // #include <pkgcraft.h>
 import "C"
 
-import (
-	"runtime"
-)
-
 type EbuildRepo struct {
 	*BaseRepo
 }
 
-type repo_ebuild_pkg_iter struct {
-	ptr *C.RepoPkgIter
-	next *EbuildPkg
-}
+// Return a channel iterating over the packages of a repo.
+func (r *EbuildRepo) Pkgs() <-chan *EbuildPkg {
+	pkgs := make(chan *EbuildPkg)
 
-// Return true if the repo iterator contains another package, false otherwise.
-func (i *repo_ebuild_pkg_iter) HasNext() bool {
-	ptr := C.pkgcraft_repo_iter_next(i.ptr)
-	if ptr == nil {
-		i.next = nil
-		return false
-	} else {
-		pkg := pkg_from_ptr(ptr)
-		if pkg.format == PkgFormatEbuild {
-			i.next = &EbuildPkg{pkg}
-		} else {
-			panic("invalid pkg format")
+	go func() {
+		iter := C.pkgcraft_repo_iter(r.ptr)
+		for {
+			ptr := C.pkgcraft_repo_iter_next(iter)
+			if ptr != nil {
+				pkg := pkg_from_ptr(ptr)
+				if pkg.format == PkgFormatEbuild {
+					pkgs <- &EbuildPkg{pkg}
+				} else {
+					panic("invalid pkg format")
+				}
+			} else {
+				break
+			}
 		}
-		return true
-	}
-}
+		close(pkgs)
+		C.pkgcraft_repo_iter_free(iter)
+	}()
 
-// Return the next package in the iterator.
-func (i *repo_ebuild_pkg_iter) Next() *EbuildPkg {
-	return i.next
-}
-
-// Return a new package iterator for a repo.
-func (r *EbuildRepo) NewPkgIterator() Iterator[*EbuildPkg] {
-	ptr := C.pkgcraft_repo_iter(r.ptr)
-	iter := &repo_ebuild_pkg_iter{ptr, nil}
-	runtime.SetFinalizer(iter, func(i *repo_ebuild_pkg_iter) { C.pkgcraft_repo_iter_free(i.ptr) })
-	return iter
+	return pkgs
 }
