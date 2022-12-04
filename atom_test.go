102d0e71b4d99175ac9d25ac75b93cb289a24c71
Tim Harder <radhermit@gmail.com>
2022-12-04 04:57:17 -0700

    atom: add tests for BlockerFromString() and SlotOperatorFromString()

--- atom_test.go
+++ atom_test.go
@@ -280,6 +280,36 @@ func TestAtomChannels(t *testing.T) {
 	}
 }
 
+func TestBlocker(t *testing.T) {
+	valid := map[string]Blocker{"!": BlockerWeak, "!!": BlockerStrong}
+	for s, expected := range valid {
+		v, err := BlockerFromString(s)
+		assert.Equal(t, v, expected)
+		assert.Nil(t, err)
+	}
+
+	invalid := []string{"", "!!!", "a"}
+	for _, s := range invalid {
+		_, err := BlockerFromString(s)
+		assert.NotNil(t, err)
+	}
+}
+
+func TestSlotOperator(t *testing.T) {
+	valid := map[string]SlotOperator{"=": SlotOpEqual, "*": SlotOpStar}
+	for s, expected := range valid {
+		v, err := SlotOperatorFromString(s)
+		assert.Equal(t, v, expected)
+		assert.Nil(t, err)
+	}
+
+	invalid := []string{"", "=*", "*=", "~"}
+	for _, s := range invalid {
+		_, err := SlotOperatorFromString(s)
+		assert.NotNil(t, err)
+	}
+}
+
 func BenchmarkNewAtom(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		atom, _ := NewAtom(fmt.Sprintf("=cat/pkg-%d-r2:3/4=[a,b,c]", i))
