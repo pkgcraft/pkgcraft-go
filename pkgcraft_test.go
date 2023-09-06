24ecb38992a13e752f9bcbf0ab93d3331dfe1621
Tim Harder <radhermit@gmail.com>
2023-09-06 09:20:44 -0600

    bump required pkgcraft-c version

--- pkgcraft_test.go
+++ pkgcraft_test.go
@@ -9,8 +9,8 @@ import (
 )
 
 // version requirements for pkgcraft C library
-const MIN_VERSION = "0.0.9"
-const MAX_VERSION = "0.0.9"
+const MIN_VERSION = "0.0.11"
+const MAX_VERSION = "0.0.11"
 
 func TestLibVersion(t *testing.T) {
 	ver, _ := version.NewVersion(pkgcraftLibVersion())
